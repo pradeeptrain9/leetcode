cba829768a948703a38cf6911bd578a5e5ee245d
pradeeptrain9 <71133260+pradeeptrain9@users.noreply.github.com>
2022-10-31 13:16:25 +0530

    Time: 19 ms (85.00%), Space: 17.4 MB (85.40%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        for(int i=0;i<matrix[0].size()-1;i++){
+                int j=i;
+                int r=0;
+                int check=matrix[0][i];
+        while(r<matrix.size() && j<matrix[0].size()){
+             //cout<<check;
+            if(matrix[r][j]==check)
+                   cout<<"";
+            else
+                return false;
+            r++,j++;
+           
+        }
+}
+    for(int i=1;i<matrix.size()-1;i++){
+                int j=i;
+                int r=0;
+                int check=matrix[i][0];
+             
+        while(j<matrix.size() && r<matrix[0].size()){
+             //cout<<check<<endl;
+            if(matrix[j][r]==check)
+                cout<<"";
+            else
+                return false;
+            j++;
+            r++;
+        }
+}
+    return true;
+    }
+};
