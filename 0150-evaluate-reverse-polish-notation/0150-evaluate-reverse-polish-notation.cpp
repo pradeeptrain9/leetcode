5b7a28b2c93f6c5dbe08142b86289adf0f0479d5
pradeeptrain9 <71133260+pradeeptrain9@users.noreply.github.com>
2023-01-04 11:53:09 +0530

    Time: 7 ms (94.17%), Space: 12 MB (76.99%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        stack<int>s;
-        for(auto x:tokens)
+        stack<int>nums;
+        for(auto exp : tokens)
         {
-            
-            if( x.size()==1 && (x[0]=='+' || x[0]=='-' || x[0]=='/' || x[0]=='*'))
+            if(exp.size()==1 && (exp[0]=='+' || exp[0]=='-' || exp[0]=='/' || exp[0]=='*'))
             {
-                int first = s.top();
-                s.pop();
-                int second = s.top();
-                s.pop();
-                if(x[0]=='+') s.push(first+second);
-                else if(x[0]=='-') s.push(second-first);
-                else if(x[0]=='*') s.push(second*first);
-                else s.push(second/first);
+                int second = nums.top();
+                nums.pop();
+                int first = nums.top();
+                nums.pop();
+                if(exp[0]=='+') nums.push(first+second);
+                else if(exp[0]=='-') nums.push(first-second);
+                else if(exp[0]=='/') nums.push(first/second);
+                else nums.push(first*second);
             }
-            else 
-            {
-                s.push(stoi(x));
-            } 
-               //cout<<"x";  
-           }
-        return s.top();
+            else nums.push(stoi(exp));
+        }
+        
+        return nums.top();
     }
 };
